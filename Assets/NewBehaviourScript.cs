61de19be78618c04dbff244712dd1f62c42d2f9b
Mukarrab <133678237+CodeChasse@users.noreply.github.com>
2023-05-25 15:21:03 -0700

    2nd game files

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float keyInput;
+    public float newkey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up*2 , ForceMode.VelocityChange);
+        }
+
+        keyInput = Input.GetAxis("Horizontal");
+        //newkey = Input.GetAxis("Vertical");
+        GetComponent<Rigidbody>().velocity= new Vector3(keyInput , GetComponent<Rigidbody>().velocity.y ,0);
+       // GetComponent<Rigidbody>().velocity= new Vector3(0 ,newkey ,0);
+
+    }
+}
